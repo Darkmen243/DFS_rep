6eb8617a615f123c1e409ddf1cd12a5b1d485cef
Darkmen243 <evseenko.vowa@yandex.ru>
2024-03-30 23:27:12 +0300

    Modified DFS algorithm to return path length

--- dfs.py
+++ dfs.py
@@ -1,22 +1,27 @@
-def dfs(graph, start):
+def dfs_with_length_and_path(graph, start, end):
     visited = set()
-    stack = [start]
-    path = []
+    stack = [(start, 0, [start])]  
 
     while stack:
-        vertex = stack.pop()
+        vertex, length, path = stack.pop()
+        if vertex == end:
+            return length, path  
         if vertex not in visited:
             visited.add(vertex)
-            path.append(vertex)
-            stack.extend(graph[vertex] - visited)
+            for neighbor in graph[vertex] - visited:
+                new_path = path + [neighbor]  
+                stack.append((neighbor, length + 1, new_path))
 
-    return path
+    return -1, []  
 
 graph = {
-    1: {3},
-    2: {4},
+    1: {2, 3},
+    2: {1, 4},
     3: {1},
     4: {2}
 }
 start_vertex = 1
-print(dfs(graph, start_vertex))  
+end_vertex = 4
+length, path = dfs_with_length_and_path(graph, start_vertex, end_vertex)
+print("Length of the shortest path:", length)
+print("Shortest path:", path)
